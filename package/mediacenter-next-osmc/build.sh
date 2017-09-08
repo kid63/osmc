9041979a015c02438f49ac5657725483047ddad6
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-09-08 02:25:52 +0100

    Remove broken game addon

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -417,7 +417,7 @@ game.libretro.beetle-lynx game.libretro.beetle-pce-fast game.libretro.fmsx \
 game.libretro.beetle-pcfx game.libretro.beetle-psx game.libretro.beetle-supergrafx game.libretro.beetle-vb \
 game.libretro.beetle-wswan game.libretro.bnes \
 game.libretro.bsnes-mercury-balanced game.libretro.bsnes-mercury-performance game.libretro.cap32 \
-game.libretro.dosbox game.libretro.fbalpha2012 game.libretro.fceumm \
+game.libretro.dosbox game.libretro.fbalpha2012 \
 game.libretro.fuse game.libretro.picodrive game.libretro.beetle-ngp \
 game.libretro.gambatte game.libretro.gw game.libretro.prboom game.libretro.handy game.libretro.hatari \
 game.libretro.lutro game.libretro.meteor game.libretro.mgba game.libretro.nestopia game.libretro.nx \
@@ -425,7 +425,7 @@ game.libretro.o2em game.libretro.pokemini game.libretro.prosystem game.libretro.
 game.libretro.snes9x game.libretro.snes9x2002 game.libretro.snes9x2010 \
 game.libretro.stella game.libretro.tgbdual game.libretro.tyrquake \
 game.libretro.vbam game.libretro.vecx game.libretro.virtualjaguar game.libretro.yabause game.libretro.dinothaw game.libretro.genplus"
-	# game.moonlight "
+	# game.moonlight game.libretro.fceumm"
 	#Broken game.libretro.reicast game.libretro.pcem game.libretro.vba-next 	
 	#game.libretro.mupen64plus 
 	#game.libretro.scummvm 
