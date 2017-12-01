1764130b3fce3770044c215442520d93fb1fd902
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-12-01 12:52:30 +0000

    Update newclock5 version

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -8,7 +8,7 @@
 #Use newclock5 for rbp1/2 builds
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ] || [ "$1" == "atv" ] || [ "$1" == "pc" ]   
 then
-pull_source "https://github.com/popcornmix/xbmc/archive/a0f0d0ebf0befc44144a1e5a64e272c721dee0d3.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/popcornmix/xbmc/archive/9c5e521f568f82a64800db09659391a61f13404f.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 
 #use xbmc top of tree for vero builds
