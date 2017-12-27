efe04e04d6b6cdd40e089bb41392d3bd19dd0e0f
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-12-27 19:57:41 +0000

    Remove GLESv3 from FindOpenGLES

--- package/mediacenter-next-osmc/patches/all-112-remove-glesv3-from-FindOpenGLES.cmake
+++ package/mediacenter-next-osmc/patches/all-112-remove-glesv3-from-FindOpenGLES.cmake
@@ -1,17 +1,17 @@
-From 1049e8fc578b7b85af1d321674d54cf83cec65b4 Mon Sep 17 00:00:00 2001
+From 5a95fc32d91a1ea19ef037ad3b32afaaed242e86 Mon Sep 17 00:00:00 2001
 From: Greg McCarthy <greg@gjmccarthy.co.uk>
-Date: Wed, 27 Dec 2017 19:30:05 +0000
+Date: Wed, 27 Dec 2017 19:39:27 +0000
 Subject: [PATCH] Remove GLESv3 from FindOpenGLES
 
 ---
- cmake/modules/FindOpenGLES.cmake | 1 -
- 1 file changed, 1 deletion(-)
+ cmake/modules/FindOpenGLES.cmake | 3 ---
+ 1 file changed, 3 deletions(-)
 
 diff --git a/cmake/modules/FindOpenGLES.cmake b/cmake/modules/FindOpenGLES.cmake
-index fac21bc4e8..2d475a97c4 100644
+index fac21bc4e8..d21d8f73fd 100644
 --- a/cmake/modules/FindOpenGLES.cmake
 +++ b/cmake/modules/FindOpenGLES.cmake
-@@ -31,7 +31,6 @@ else()
+@@ -31,14 +31,11 @@ else()
    set(OPENGLES_INCLUDE_DIR ${OPENGLES_gl_LIBRARY}/Headers)
  endif()
  
@@ -19,6 +19,13 @@ index fac21bc4e8..2d475a97c4 100644
  
  include(FindPackageHandleStandardArgs)
  find_package_handle_standard_args(OpenGLES
+                                   REQUIRED_VARS OPENGLES_gl_LIBRARY OPENGLES_INCLUDE_DIR)
+ 
+-find_path(OPENGLES3_INCLUDE_DIR GLES3/gl3.h
+-                                PATHS ${PC_OPENGLES_INCLUDEDIR})
+ 
+ if(OPENGLES_FOUND)
+   set(OPENGLES_LIBRARIES ${OPENGLES_gl_LIBRARY})
 -- 
 2.12.3
 
