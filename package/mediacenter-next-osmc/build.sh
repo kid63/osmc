864398e70555f1ec461a24d6dbf967004dfa3394
Greg McCarthy <greg@gjmccarthy.co.uk>
2018-01-28 10:37:05 +0000

    Fix typo in buid script

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -8,7 +8,7 @@
 #Use newclock5 for rbp1/2 builds
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]    
 then
-pull_source "https://github.com/popcornmix/xbmc/archive/dcc711835ff4014bec2a0774aa616469529b73d2.gz" "$(pwd)/src"
+pull_source "https://github.com/popcornmix/xbmc/archive/dcc711835ff4014bec2a0774aa616469529b73d2.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 
 #use xbmc top of tree for vero builds
