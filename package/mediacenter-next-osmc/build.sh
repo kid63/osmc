7ed975ab79b8e59936c008ab6d96fb11225a2afc
Greg McCarthy <greg@gjmccarthy.co.uk>
2018-03-21 10:08:19 +0000

    Bump nightly build

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -8,12 +8,12 @@
 #Use newclock5 for rbp1/2 builds
 if [ "$1" == "rbp1" ] || [ "$1" == "rbp2" ]    
 then
-pull_source "https://github.com/popcornmix/xbmc/archive/8e37101b61a9d2795f45345c78d3fbfca656298a.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/popcornmix/xbmc/archive/b1e4016621a66497770b5f55c6ac3aad601bba52.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 
 #use xbmc top of tree for vero builds
 else
-pull_source "https://github.com/xbmc/xbmc/archive/cf333ccd73f7a18de1378fc6665f913543c96e51.tar.gz" "$(pwd)/src"
+pull_source "https://github.com/xbmc/xbmc/archive/384bd17e6cfcde9e6e1dee6f8d8cccc4c7d07516.tar.gz" "$(pwd)/src"
 API_VERSION="18"
 fi
 if [ $? != 0 ]; then echo -e "Error fetching Kodi source" && exit 1; fi
