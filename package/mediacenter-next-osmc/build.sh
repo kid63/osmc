d3c474f04de0fb13a96a5ea25d49c9531cbaffa4
Greg McCarthy <greg@gjmccarthy.co.uk>
2018-09-05 21:08:01 +0100

    Temp removal of game.libretro.dosbox due to build issue

--- package/mediacenter-next-osmc/build.sh
+++ package/mediacenter-next-osmc/build.sh
@@ -336,13 +336,13 @@ game.libretro.beetle-gba game.libretro.fceumm game.libretro.gw \
 game.libretro.beetle-pce-fast \
 game.libretro.bnes game.libretro.nestopia \
 game.libretro.bsnes-mercury-balanced  \
-game.libretro.dosbox game.libretro.bluemsx \
+game.libretro.bluemsx \
 game.libretro.gambatte game.libretro.beetle-bsnes \
 game.libretro.meteor game.libretro.mgba game.libretro.vba-next \
 game.libretro.quicknes game.libretro.scummvm \
 game.libretro.snes9x game.libretro.stella \
 game.libretro.vbam game.libretro.yabause game.libretro.genplus"
-# Remove  game.libretro.mame game.libretro.bsnes-mercury-accuracy game.libretro.mupen64plus game.libretro.pcsx-rearmed
+# Remove  game.libretro.dosbox game.libretro.mame game.libretro.bsnes-mercury-accuracy game.libretro.mupen64plus game.libretro.pcsx-rearmed
 	ADDONS_PVR="pvr.sledovanitv.cz pvr.argustv pvr.mythtv pvr.hts pvr.pctv pvr.stalker pvr.filmon pvr.octonet pvr.zattoo pvr.vbox pvr.wmc pvr.nextpvr pvr.njoy pvr.teleboy pvr.vdr.vnsi pvr.vuplus pvr.dvbviewer pvr.dvblink pvr.hdhomerun pvr.iptvsimple pvr.demo pvr.mediaportal.tvserver vfs.libarchive vfs.rar vfs.sftp" 
 	ADDONS_SCREENSAVERS="screensaver.asteroids screensaver.biogenesis screensaver.greynetic screensaver.matrixtrails screensaver.pingpong screensaver.pyro screensavers.rsxs screensaver.stars screensaver.shadertoy"
 	ADDONS_VISUALIZATIONS="visualization.fishbmc visualization.goom visualization.projectm visualization.shadertoy visualization.spectrum visualization.vsxu visualization.waveform"
